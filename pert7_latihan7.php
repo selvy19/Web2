a11211e861f823cff8b7a1262270a2c642928bca
selvy19 <78544505+selvy19@users.noreply.github.com>
2021-03-17 19:24:59 +0700

    Add files via upload

---
+++ pert7_latihan7.php
@@ -0,0 +1,12 @@
+<HTML>
+<HEAD>
+<TITLE> Penggunaan Join </TITLE>
+</HEAD>
+<BODY>
+<?php
+$var = array (' Tanggal 17 ' , ' Bulan 05 ' , ' Tahun 2010 ');
+$tanggal = join(" - ", $var);
+echo "$tanggal";
+?>
+</BODY>
+</HTML> 
